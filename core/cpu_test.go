7410cf08677184fb1873e669acf0536ea809e016
Christophe Fajardo <christophe.fajardo@eureka-technology.com>
2016-09-06 09:56:09 +0200

    LD (BC) A unit test

--- core/cpu_test.go
+++ core/cpu_test.go
@@ -40,3 +40,26 @@ func TestLdBcNn(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestLdBcpA(t *testing.T) {
+	system := New()
+	system.cpu.registers.A = 0xF0
+	system.cpu.registers.B = byte(workRAMBank0BaseAddress >> 8)
+	system.cpu.registers.C = byte(workRAMBank0BaseAddress & 0x00FF)
+	system.cpu.mmu.writeByte(workRAMBank0BaseAddress, 0xF0)
+	system.cpu.ProgramCounter = romBank00BaseAddress
+	system.cpu.mmu.writeByte(romBank00BaseAddress, 0x2)
+	system.Execute()
+
+	instruction := (*system.cpu.instructionSet)[0x2]
+	if system.cpu.ProgramCounter != uint16(instruction.length) {
+		t.Logf("system.cpu.ProgramCounter = %d, expected = %d", system.cpu.ProgramCounter, instruction.length)
+		t.Fail()
+	}
+
+	value := system.cpu.mmu.readByte(workRAMBank0BaseAddress)
+	if value != 0xF0 {
+		t.Logf("system.cpu.mmu.memory[0xC000] = %d, expected = %d", value, 0xF0)
+		t.Fail()
+	}
+}

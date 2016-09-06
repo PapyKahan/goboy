4e03e728175cd5a80fddb582eeaeb77a70f2e939
Christophe Fajardo <christophe.fajardo@eureka-technology.com>
2016-09-06 08:06:52 +0200

    ld bc nn unit test

--- core/cpu_test.go
+++ core/cpu_test.go
@@ -1,6 +1,9 @@
 package core
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNop(t *testing.T) {
 	system := New()
@@ -8,23 +11,34 @@ func TestNop(t *testing.T) {
 	instruction := (*system.cpu.instructionSet)[0x0]
 	instruction.execute()
 	if system.cpu.ProgramCounter != 1 {
-		t.Logf("system.cpu.ProgramCounter = %d, expected = 2", system.cpu.ProgramCounter)
+		t.Logf("system.cpu.ProgramCounter = %d, expected = 1", system.cpu.ProgramCounter)
 		t.Fail()
 	}
 }
 
-// func TestLdBcNn(t *testing.T) {
-// 	system := New()
-// 	system.cpu.registers.B = 0
-// 	system.cpu.registers.C = 0
-// 	system.cpu.ProgramCounter = 1
+func TestLdBcNn(t *testing.T) {
+	system := New()
+	system.cpu.registers.B = 0
+	system.cpu.registers.C = 0
+	system.cpu.ProgramCounter = 0
+	system.cpu.mmu.writeWord(0x0, 0xFF0F)
 
-// 	instruction := (*system.cpu.instructionSet)[0x1]
-// 	instruction.execute()
-// 	fmt.Println("initialize instruciton set")
+	instruction := (*system.cpu.instructionSet)[0x1]
+	instruction.execute()
+	fmt.Println("initialize instruciton set")
 
-// 	if system.cpu.ProgramCounter != uint16(instruction.length) {
-// 		t.Logf("system.cpu.ProgramCounter = %d, expected = %d", system.cpu.ProgramCounter, 3)
-// 		t.Fail()
-// 	}
-// }
+	if system.cpu.ProgramCounter != uint16(instruction.length-1) {
+		t.Logf("system.cpu.ProgramCounter = %d, expected = %d", system.cpu.ProgramCounter, 2)
+		t.Fail()
+	}
+
+	if system.cpu.registers.B != 0xFF {
+		t.Logf("system.cpu.registers.B = %d, expected = %d", system.cpu.registers.B, 0xFF)
+		t.Fail()
+	}
+
+	if system.cpu.registers.C != 0x0F {
+		t.Logf("system.cpu.registers.C = %d, expected = %d", system.cpu.registers.C, 0x0F)
+		t.Fail()
+	}
+}
